da8ddc37202fdd32b3ad86af0a6a318bd01c1c46
Vinay Kumar <vinaykumar2491@users.noreply.github.com>
2016-03-02 15:37:09 +0530

    Added files via upload

---
+++ Codes/Ch_02/ch02_02.java
@@ -0,0 +1,11 @@
+class ch02_02
+{
+	public static void main(String args[]){
+		int num; 
+		num = 100;
+		System.out.println("This is num: " + num);
+		num = num * 2;
+		System.out.print("The valuse of num * 2 = ");
+		System.out.println(num);
+	}
+}
