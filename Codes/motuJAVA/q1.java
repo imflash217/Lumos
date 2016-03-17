24afb40952a9d22a60e5131b6053a48aeaaa848b
Vinay Kumar <vinaykumar2491@users.noreply.github.com>
2016-03-17 15:58:32 +0530

    Create q1.java

---
+++ Codes/motuJAVA/q1.java
@@ -0,0 +1,10 @@
+///////////// Question //////////////////////
+//  1.Is java case-sensitive?
+//  2.Read the doc on Sun's Code Conventions and write few note points as an ans. to this question.
+//  3.What are legal identifiers?
+
+///////////// Answer //////////////////////
+
+///////////// Assessment Program //////////
+
+
