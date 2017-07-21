3f4bdf367353de5f9d77918500c98d123fa70816
vinaykumar2491 <vinaykumar2491@users.noreply.github.com>
2017-07-21 17:45:21 +0530

    lp2thw_lesson14_verFinal

--- Project_Python/learn_python_thw/lesson14.py
+++ Project_Python/learn_python_thw/lesson14.py
@@ -1,1 +1,27 @@
 # lesson14.py
+
+from sys import argv
+
+script, user_name = argv
+prompt = '>'
+
+print "Hi %s, I\'m the %s script." % (user_name, script)
+
+print "I would like to ask you few question?"
+print 'Do you like me %s' % user_name
+likes = raw_input(prompt)
+
+print 'WHere do you live %s?' % user_name
+lives = raw_input(prompt)
+
+print 'What kind of computer do you have?'
+computer = raw_input(prompt)
+
+print """
+Alright, So you said %r about liking me. 
+You live in %r. Not sure where that is :P
+And you have a %r computer. Nice.
+""" % (likes, lives, computer)
+
+
+
