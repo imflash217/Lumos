e9077d5ea599998d0a6f5b6df5c3916042461a6d
vinaykumar2491 <vinaykumar2491@users.noreply.github.com>
2017-07-23 20:33:03 +0530

    lesson33 _verFinal2

--- Project_Python/learn_python_thw/lesson33.py
+++ Project_Python/learn_python_thw/lesson33.py
@@ -1,19 +1,19 @@
 
-def append_list(m_list, m_roof):
+def append_list(m_list, m_roof, m_increment):
 	i = len(m_list)+1
-	numbers = m_list
+	# numbers = m_list
 
 	while i < m_roof :
 		print 'At the top i is %d' % i 
-		numbers.append(i)
-		i += 1
-		print 'Numbers now : ', numbers
+		m_list.append(i)
+		i += m_increment
+		print 'Numbers now : ', m_list
 		print 'At the bottom i is : %d' % i 
 
-	print 'Finally the number[]: '
-	for num in numbers:
+	print 'Finally the m_list[]: '
+	for num in m_list:
 		print num
 
 
 m_list = ['a', 'b', 'c']
-append_list(m_list, 13)
+append_list(m_list, 13, 3)
